4f34a06fc11183a1cb418b1b39f4fc967ab2e05f
marvin <sea-son@foxmail.com>
2025-02-13 18:11:12 +0800

    fix: 弹窗弹出优化

--- blog/ai-input/components/ai-input.tsx
+++ blog/ai-input/components/ai-input.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
+import styles from './styles.module.css';
 type Attachment = {
     type: 'tool' | 'file',
+    symbol: string,
     label: string,
     value: string
 }
@@ -26,9 +28,8 @@ const CoreInput = ({
             }
         </div>
         <div>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-            <button onClick={() => onSend(value, attachments)}>Send</button>
-            <span>Open the console log to view the result</span>
+            <input className={styles.input} type="text" value={value} onChange={e => setValue(e.target.value)} />
+            <button className={styles.sendBtn} onClick={() => onSend(value, attachments)}>Send</button>
         </div>
         <div>
             {attachmentRender(value, attactment => {
@@ -42,27 +43,36 @@ const CoreInput = ({
     </>
 }
 
-
+const attachments: Attachment[] = [
+    {
+        type: 'tool',
+        symbol: '@',
+        label: 'tool 1',
+        value: 'tool1'
+    }, {
+        type: 'tool',
+        symbol: '@',
+        label: 'tool 2',
+        value: 'tool2'
+    }, {
+        type: 'file',
+        symbol: '#',
+        label: 'file 1',
+        value: 'file1'
+    },
+]
 export default function AIInput() {
-
-    const attachments: Attachment[] = [
-        {
-            type: 'tool',
-            label: 'tool 1',
-            value: 'tool1'
-        }, {
-            type: 'tool',
-            label: 'tool 2',
-            value: 'tool2'
-        }, {
-            type: 'file',
-            label: 'file 1',
-            value: 'file1'
-        },
-    ]
+    const groupedAttachments = useMemo(() => {
+        // @ts-ignore
+        return Object.groupBy(attachments, ({ type }) => type) as Record<Attachment['type'], Attachment[]>;
+    }, [attachments])
+    const [activedKey, setActivedKey] = useState<Attachment['type']>(undefined);
 
     return <CoreInput
-        onSend={console.log}
+        onSend={(...props) => {
+            alert('Open the console log to view the result');
+            console.log(props)
+        }}
         attachmentHandler={(attactment, setAttachments) => {
             setAttachments(prev => {
                 if (prev.find(a => a.value === attactment.value)) {
@@ -72,25 +82,37 @@ export default function AIInput() {
             })
         }}
         attachmentRender={(value, onSelect) => {
-            return <>
+            return <div className={styles.attachmentGroup}>
                 {
-                    attachments.map(attachment => {
-                        return <strong onClick={() => {
-                            onSelect(attachment)
-                        }} key={attachment.value}>{attachment.label}</strong>
+                    Object.entries(groupedAttachments).map(([key, value]) => {
+                        return <span className={styles.attachment} key={key} onClick={() => {
+                            setActivedKey(key as Attachment['type'])
+                        }}>
+                            {groupedAttachments[key][0].symbol}
+                        </span>
                     })
-
                 }
-            </>
+                {
+                    activedKey && <div className={styles.attachmentListContainer}>
+                        {
+                            groupedAttachments[activedKey].map(attachment => {
+                                return <span onClick={() => {
+                                    onSelect(attachment)
+                                }} key={attachment.value}>{attachment.label}</span>
+                            })
+                        }
+                    </div>
+                }
+            </div>
         }}
         attachmentDisplayer={(attactments, setAttachments) => {
-            return <>
+            return <div className={styles.attachmentGroup}>
                 {attactments.map(attachment => {
-                    return <span key={attachment.value} onClick={() => {
+                    return <span className={styles.attachment} style={{ backgroundColor: 'lightgreen' }} key={attachment.value} onClick={() => {
                         setAttachments(prev => prev.filter(a => a.value !== attachment.value))
                     }}>{attachment.label}</span>
                 })}
-            </>
+            </div>
         }}
     />
 }
