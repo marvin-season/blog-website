c381d7eaec2de1ee0e39a19b903ccf7c8d11378d
marvin <sea-son@foxmail.com>
2025-02-13 14:25:52 +0800

    feat: base react compont in mdx

---
+++ blog/react-develop/attach-selector.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+
+const AttachSelector = () => {
+    useEffect(() => {
+        console.log('AttachSelector')
+    }, [])
+    return <>
+        AttachSelector
+    </>
+}
+
+export default AttachSelector
