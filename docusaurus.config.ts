887bd96225c2756670ff1f421c68297de3914727
marvin <sea-son@foxmail.com>
2025-02-14 17:43:50 +0800

    fix: 配置显示所有的文章

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,6 +52,7 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
+          blogSidebarCount: 'ALL',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
