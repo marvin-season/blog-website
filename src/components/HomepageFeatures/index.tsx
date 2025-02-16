b9972fa0994419564321ee67ba9278fa03429489
marvin <sea-son@foxmail.com>
2025-02-16 18:43:52 +0800

    feat: 首页

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: '朝花夕拾',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+       Gathering flowers in the morning, reminiscing in the evening
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: '兴尽悲来，识盈虚之有数。',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        When joy ends and sorrow comes, one understands the cyclical nature of fortune
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: '东隅已逝，桑榆非晚。',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Though the morning sun has set, the evening glow is still bright.
       </>
     ),
   },
