838ebe9a48aded84b5ebadddb800d7a987660bab
marvin <sea-son@foxmail.com>
2025-02-18 10:56:36 +0800

    feat: @

--- docs/rich-editor/editor.tsx
+++ docs/rich-editor/editor.tsx
@@ -3,6 +3,8 @@ import { StarterKit } from '@tiptap/starter-kit'
 // import { Markdown } from 'tiptap-markdown'
 import InlinePlaceholder from './inline-placeholder'
 import { serialize } from './utils'
+import Tippy from '@tippyjs/react'
+import { useState } from 'react'
 
 export const useEditorProps = (content: string | JSONContent) => {
     return {
@@ -41,4 +43,40 @@ export const useEditorProps = (content: string | JSONContent) => {
 
 const handleSave = (json: JSONContent) => {
     alert(serialize(json))
+}
+
+const templateList = [
+    "我是{{宫本武藏}},我的工作是 {{吃饭睡觉打拳击}}。",
+    "请帮我查询 {{昨天}}的日程"
+]
+export const Trigger = () => {
+    const [activedKey, setActivedKey] = useState<string>(undefined);
+    return <>
+        <Tippy
+            delay={100}
+            duration={0}
+            content={<div className={""}>
+                {
+                    templateList.map((item, index) => {
+                        return <div onClick={() => {
+                            console.log('click', item)
+                        }} key={index}>{item}</div>
+                    })
+                }
+            </div>}
+            interactive
+            placement={'top-start'} // 将弹窗位置设置为底部
+            visible={activedKey === '@'}
+            onClickOutside={() => setActivedKey(null)} // 点击外部关闭弹窗
+        >
+            <div className='cursor-pointer p-4 bg-white w-full rounded-[12px] shadow text-blue-500'>
+                <span onClick={() => {
+                    setActivedKey('@')
+                }}>
+                    {'@'}
+                </span>
+            </div>
+
+        </Tippy>
+    </>
 }
--- docs/rich-editor/index.tsx
+++ docs/rich-editor/index.tsx
@@ -1,12 +1,13 @@
 import { EditorProvider } from "@tiptap/react";
-import { useEditorProps } from "./editor";
+import { Trigger, useEditorProps } from "./editor";
 import { deserialize } from "./utils";
 
 function App() {
   const editorProps = useEditorProps(deserialize("我是{{宫本武藏}},我的工作是 {{吃饭睡觉打拳击}}。"))
-  return <div className="bg-[#f5f1f1] rounded-4xl flex flex-col justify-center items-center gap-4 p-8">
+  return <div className="bg-[#f5f1f1] rounded-4xl flex flex-col justify-center items-center gap-4 p-8 pb-4">
     <EditorProvider editable={true} {...editorProps}>
-      <span>{'CTRL/Command + Enter to send'}</span>
+        <Trigger />
+        <div className="text-gray-400 text-sm">{'CTRL/Command + Enter or Typing @'}</div>
     </EditorProvider>
   </div>;
 }
