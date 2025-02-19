03c1af1cd56d9da46bb5fc4ebc41c16e68c2880e
marvin <sea-son@foxmail.com>
2025-02-19 18:19:49 +0800

    fix: close 失效

--- src/components/rich-editor/trigger.tsx
+++ src/components/rich-editor/trigger.tsx
@@ -22,33 +22,35 @@ export const Trigger = ({ ref }) => {
     }, [])
 
     return <>
-        {/* trigger @ */}
-        <Tippy
-            delay={100}
-            className='border border-blue-300 backdrop-blur backdrop-opacity-80 p-4 rounded-[12px] shadow text-sm'
-            duration={0}
-            content={<div className={"flex flex-col gap-2"}>
-                {
-                    templateList.map((item, index) => {
-                        return <div className='truncate cursor-pointer hover:text-blue-400' onClick={() => {
-                            editor.commands.setContent(deserialize(item))
-                        }} key={index}>{item}</div>
-                    })
-                }
-            </div>}
-            interactive
-            placement={'top-start'} // 将弹窗位置设置为底部
-            visible={triggerKey === '@'}
-            onClickOutside={() => setTriggerKey(null)} // 点击外部关闭弹窗
-        >
-            <div className='cursor-pointer px-4 py-2 bg-white w-full rounded-[12px] shadow text-blue-500'>
+        <div className='cursor-pointer px-4 py-2 bg-white w-full rounded-[12px] shadow text-blue-500'>
+            <Tippy
+                delay={100}
+                className='border border-blue-300 backdrop-blur backdrop-opacity-80 p-4 rounded-[12px] shadow text-sm'
+                duration={0}
+                content={<div className={"flex flex-col gap-2"}>
+                    {
+                        templateList.map((item, index) => {
+                            return <div className='truncate cursor-pointer hover:text-blue-400' onClick={() => {
+                                editor.commands.setContent(deserialize(item))
+                            }} key={index}>{item}</div>
+                        })
+                    }
+                </div>}
+                interactive
+                placement={'top-start'} // 将弹窗位置设置为底部
+                visible={triggerKey === '@'}
+                onClickOutside={() => setTriggerKey(null)} // 点击外部关闭弹窗
+            >
                 <span onClick={() => {
                     setTriggerKey(triggerKey === '@' ? null : '@')
                 }}>
                     {'@'}
                 </span>
-            </div>
 
-        </Tippy>
+
+            </Tippy>
+        </div>
+        {/* trigger @ */}
+
     </>
 }
