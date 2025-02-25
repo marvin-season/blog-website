fd9776d6648318b5ba86521fce2a896503e9a408
marvin <sea-son@foxmail.com>
2025-02-25 09:28:03 +0800

    feat: publish er

--- src/process.handle.js
+++ src/process.handle.js
@@ -1,80 +1,21 @@
-class Producer {
-  constructor(coreProcessor) {
-    this.coreProcessor = coreProcessor
-  }
-  async produce(message) {
-    this.coreProcessor.queue.push({
-      middleware: (message) => {
-        return message
-      },
-      stream: false,
-    })
-    this.coreProcessor.handle(message)
-  }
+const queue = []
+
+function publisher(data) {
+  queue.forEach((resolve) => {
+    resolve(data)
+  })
 }
 
-class Consumer {
-  constructor(coreProcessor) {
-    this.coreProcessor = coreProcessor
-  }
-  async consume(message) {
-    console.log('consume', message)
-  }
+async function subscriber() {
+  const result = await new Promise((resolve) => {
+    queue.push(resolve)
+  })
+  console.log('subscriber', result)
 }
 
-class CoreProcessor {
-  queue = [] // middleware queue
-  constructor() {
-    this.producer = new Producer(this)
-    this.consumer = new Consumer(this)
-  }
+subscriber()
+subscriber()
+setTimeout(() => {
+  publisher('hi')
+}, 1000)
 
-  use(middleware, { stream = false } = {}) {
-    this.queue.push({
-      middleware,
-      stream,
-    })
-  }
-
-  async handleStream(message, middleware) {
-    const iter = middleware(message)
-    let msg = ''
-    for await (const item of iter) {
-      msg += item
-      this.consumer.consume(item)
-    }
-    return msg
-  }
-
-  async handle(msg) {
-    let message = msg
-    for (const { middleware, stream } of this.queue) {
-      if (stream) {
-        message = await this.handleStream(message, middleware)
-      } else {
-        message = await middleware(message)
-      }
-    }
-    this.consumer.consume(message)
-  }
-}
-
-const coreProcessor = new CoreProcessor()
-
-coreProcessor.use(
-  async function* (promiseMessage) {
-    for await (const item of promiseMessage) {
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-      yield item
-    }
-  },
-  {
-    stream: true,
-  }
-)
-
-coreProcessor.use((message) => {
-  console.log('full log', message)
-  return message
-})
-coreProcessor.producer.produce('hi')
