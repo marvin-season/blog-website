9f3e64774b99d15c1813a3edde304a000907d3a3
marvin <sea-son@foxmail.com>
2025-03-05 14:21:42 +0800

    template: MDX React

---
+++ src/components/notification/index.tsx
@@ -0,0 +1,9 @@
+export { default as Notification } from "./notification";
+import UseCase from './use-case'
+
+
+export const App = () => {
+    return <>
+        <UseCase/>
+    </>;
+};
---
+++ src/components/notification/use-case.tsx
@@ -0,0 +1,6 @@
+export default function useCase() {
+    return <>
+        App
+    </>;
+
+}
