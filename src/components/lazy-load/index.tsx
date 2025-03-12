b90f240cc2afe64c80469c73813f4ba7e55d9368
marvin <sea-son@foxmail.com>
2025-03-12 11:40:12 +0800

    blog: lazy load blog ok

--- src/components/lazy-load/index.tsx
+++ src/components/lazy-load/index.tsx
@@ -1,15 +1,56 @@
+import { useEffect, useRef, useState } from "react";
+
 export default function App() {
+    const [row, setRow] = useState(1000000);
     return <>
-    {"Vertical"}
-        <Vertical />
+        <div>
+            {"Vertical: "}
+            {row}
+        </div>
+        <Vertical row={row} />
     </>;
 }
 
-export const Vertical = ({ count = 100 }) => {
-    return <div className={"h-[300px] overflow-y-auto border"}>
+export const Vertical = ({ row = 1000000 }) => {
+    const rootRef = useRef(null);
+    const intersectionObserverRef = useRef<IntersectionObserver>(null);
+    const [pageCeil, setPageCeil] = useState(5);
+    const [count, setCount] = useState(0);
+    const countRef = useRef(0);
+    useEffect(() => {
+        console.log(count);
+        intersectionObserverRef.current = new IntersectionObserver((entries, observer) => {
+            if (entries.findIndex(item => item.isIntersecting) > -1) {
+                setPageCeil(prev => {
+                    if (prev + 1 >= countRef.current) {
+                        return countRef.current;
+                    }
+                    return prev + 1;
+                });
+            }
+        }, {
+            root: rootRef.current,
+        });
+        setTimeout(() => {
+            setCount(row);
+            countRef.current = row;
+        });
+
+        return () => {
+            intersectionObserverRef.current?.disconnect();
+        };
+    }, [row]);
+    return <div className={"h-[300px] overflow-y-auto border"} ref={rootRef}>
         {
-            new Array(count).fill(0).map((_, index) => {
-                return <div key={index}>{index}</div>;
+            new Array(count).fill(0).slice(0, pageCeil).map((_, index) => {
+                return <div className={"h-[100px] border border-gray-300"} key={index} ref={(dom) => {
+                    if (!dom) return;
+                    if (index === pageCeil - 1) {
+                        intersectionObserverRef.current?.observe(dom);
+                    } else {
+                        intersectionObserverRef.current?.unobserve(dom);
+                    }
+                }}>{index}</div>;
             })
         }
     </div>;
