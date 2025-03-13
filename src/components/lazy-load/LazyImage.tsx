0fc0277ad836987ff126eb5d0543ae9d41349395
marvin <sea-son@foxmail.com>
2025-03-13 14:37:12 +0800

    fix: lazy-load

---
+++ src/components/lazy-load/LazyImage.tsx
@@ -0,0 +1,15 @@
+import Logo from '@site/static/img/logo.png'
+
+export default function LazyImage() {
+    return <>
+        <Case1/>
+
+    </>
+}
+
+function Case1(){
+    return <div className={'h-[100px] overflow-y-scroll'}>
+        <div className={'h-[3000px] bg-blue-200'}></div>
+        <img src={Logo} alt="示例图片" loading="lazy" onLoad={console.log}/>
+    </div>
+}
--- src/components/lazy-load/index.tsx
+++ src/components/lazy-load/index.tsx
@@ -1,3 +1,4 @@
 export { default as VirtualPage} from './VirtualPage'
+export { default as LazyImage} from './LazyImage'
 
 
