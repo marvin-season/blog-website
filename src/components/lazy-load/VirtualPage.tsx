d0b63393a0a43aefccbadefdf0397b397638a4d5
marvin <sea-son@foxmail.com>
2025-03-19 14:57:16 +0800

    feat: 页面虚拟化 完善

--- src/components/lazy-load/VirtualPage.tsx
+++ src/components/lazy-load/VirtualPage.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 
 export default function VirtualPage() {
-    const [row, setRow] = useState(10);
+    const [row, setRow] = useState(100);
     return (
         <>
             <div>
                 {"Vertical: "}
                 {row}
             </div>
-            <Vertical row={row} />
+            <Vertical row={row} startIndex={50} buffer={0} length={8} acceleration={1}/>
         </>
     );
 }
@@ -41,7 +41,7 @@ function createIntersectionObserver({
     return observer;
 }
 
-export const Vertical = ({ row = 1000000, startIndex = 4, length = 2, acceleration = 1, buffer = 1 }) => {
+export const Vertical = ({ row = 1000000, startIndex = 0, length = 2, acceleration = 1, buffer = 1 }) => {
     const rootRef = useRef<HTMLDivElement>(null);
     const bottomTargetRef = useRef(null);
     const topTargetRef = useRef(null);
@@ -68,6 +68,7 @@ export const Vertical = ({ row = 1000000, startIndex = 4, length = 2, accelerati
             targets: [bottomTargetRef.current, topTargetRef.current],
             onIntersecting: (entries) => {
                 entries.forEach((entry) => {
+                    console.log(entry);
                     if (entry.target === topTargetRef.current) {
                         setRange((prev) => {
                             return {
@@ -105,7 +106,8 @@ export const Vertical = ({ row = 1000000, startIndex = 4, length = 2, accelerati
                         ref={dom => {
                             if(dom) {
                                 // 滚动到顶部时，向上滚动
-                                if(range.startIndex !== 0 && index - buffer < 0 && dom.parentElement.scrollTop <= 0) {
+                                if(range.startIndex !== 0 && dom.parentElement.scrollTop <= 0) {
+                                    console.log('scroll', dom)
                                     dom.parentElement.scrollTo({
                                         top: dom.clientHeight
                                     })
