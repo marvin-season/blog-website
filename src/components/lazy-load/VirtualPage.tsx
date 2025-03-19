ee87c5196c302ca8b5f56bd2de7906c9165cf46f
marvin <sea-son@foxmail.com>
2025-03-19 15:23:11 +0800

    feat: 虚拟化 添加操作栏

--- src/components/lazy-load/VirtualPage.tsx
+++ src/components/lazy-load/VirtualPage.tsx
@@ -1,14 +1,13 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import { createIntersectionObserver } from 'aio-tool';
 export default function VirtualPage() {
-    const [row, setRow] = useState(100);
+    const [idKey, setIdKey] = useState(Date.now);
     return (
         <>
-            <div>
-                {"Vertical: "}
-                {row}
-            </div>
-            <Vertical row={row} startIndex={50} buffer={0} length={8} acceleration={5}/>
+            <button className={"bg-green-400 rounded-lg px-2 py-0.5 text-white hover:bg-green-500 cursor-pointer"} onClick={() => {
+                setIdKey(Date.now())
+            }}>Reset</button>
+            <Vertical key={idKey} row={10000} startIndex={50} buffer={2} length={5} acceleration={2}/>
         </>
     );
 }
@@ -20,8 +19,8 @@ export const Vertical = ({ row = 1000000, startIndex = 0, length = 2, accelerati
     const topTargetRef = useRef(null);
 
     const [range, setRange] = useState({
-        startIndex: startIndex - acceleration, // TODO: solve the problem of the first scroll
-        end: startIndex - acceleration + length,
+        startIndex: startIndex - acceleration + buffer, // TODO: solve the problem of the first scroll
+        end: startIndex - acceleration + buffer + length,
     });
 
     const rows = useMemo(() => {
@@ -64,43 +63,53 @@ export const Vertical = ({ row = 1000000, startIndex = 0, length = 2, accelerati
             observer?.disconnect();
         };
     }, []);
-
     return (
-        <div className={"h-[200px] overflow-y-auto border"} ref={rootRef}>
-            <div
-                ref={topTargetRef}
-                className={"text-center text-sm text-gray-500"}
-            >
-                {range.end === row ? "暂无更多" : "下拉加载更多"}
-            </div>
-            {rows.map((item, index) => {
-                return (
-                    <div
-                        ref={dom => {
-                            if(dom) {
-                                // 滚动到顶部时，向上滚动
-                                if(range.startIndex !== 0 && dom.parentElement.scrollTop <= 0) {
-                                    console.log('scroll', dom)
-                                    dom.parentElement.scrollTo({
-                                        top: dom.clientHeight * acceleration //TODO: scroll at the first time is not correct
-                                    })
+        <div>
+            <ul className={"h-[190px] overflow-y-scroll"}>
+                <li>row: {row}</li>
+                <li>startIndex: {range.startIndex}</li>
+                <li>end: {range.end}</li>
+                <li>acceleration: {acceleration}</li>
+                <li>buffer: {buffer}</li>
+                <li>rows: {JSON.stringify(rows)}</li>
+            </ul>
+            <div className={"h-[200px] overflow-y-auto border"} ref={rootRef}>
+                <div
+                    ref={topTargetRef}
+                    className={"text-center text-sm text-gray-500"}
+                >
+                    {range.end === row ? "暂无更多" : "下拉加载更多"}
+                </div>
+                {rows.map((item, index) => {
+                    return (
+                        <div
+                            ref={dom => {
+                                if(dom) {
+                                    // 滚动到顶部时，向上滚动
+                                    if(range.startIndex !== 0 && dom.parentElement.scrollTop <= 0) {
+                                        console.log('scroll', dom)
+                                        dom.parentElement.scrollTo({
+                                            top: dom.clientHeight * acceleration //TODO: scroll at the first time is not correct
+                                        })
+                                    }
                                 }
-                            }
-                        }}
-                        className={"h-[100px] border border-gray-300"}
-                        key={item}
-                    >
-                        {item}
-                    </div>
-                );
-            })}
+                            }}
+                            className={"h-[100px] border border-gray-300"}
+                            key={item}
+                        >
+                            {item}
+                        </div>
+                    );
+                })}
 
-            <div
-                ref={bottomTargetRef}
-                className={"text-center text-sm text-gray-500"}
-            >
-                {range.end === row ? "暂无更多" : "加载更多"}
+                <div
+                    ref={bottomTargetRef}
+                    className={"text-center text-sm text-gray-500"}
+                >
+                    {range.end === row ? "暂无更多" : "加载更多"}
+                </div>
             </div>
         </div>
+
     );
 };
