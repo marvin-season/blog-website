d0ab771a1bf52e4a3a8ef50b93014781b1a36dee
marvin <sea-son@foxmail.com>
2025-03-19 11:02:39 +0800

    feat: 页面虚拟化指定区间

--- src/components/lazy-load/VirtualPage.tsx
+++ src/components/lazy-load/VirtualPage.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 export default function VirtualPage() {
-    const [row, setRow] = useState(10);
+    const [row, setRow] = useState(1000000);
     return (
         <>
             <div>
                 {"Vertical: "}
                 {row}
             </div>
-            <Vertical row={row} />
+            <Vertical row={row} startIndex={10} length={5} />
         </>
     );
 }
@@ -41,24 +41,63 @@ function createIntersectionObserver({
     return observer;
 }
 
-export const Vertical = ({ row = 1000000 }) => {
+export const Vertical = ({ row = 1000000, startIndex = 0, length = 5, acceleration = 2 }) => {
     const rootRef = useRef(null);
-    const anchorRef = useRef(null);
+    const bottomTargetRef = useRef(null);
+    const topTargetRef = useRef(null);
 
-    const [pageCeil, setPageCeil] = useState(5);
+    const [range, setRange] = useState({
+        startIndex,
+        endIndex: startIndex + length,
+    });
+
+    const rows = useMemo(() => {
+        return new Array(row)
+            .fill(0)
+            .map((_, i) => i)
+            .slice(range.startIndex, range.endIndex);
+    }, [row, range]);
+
+    console.log(rows);
 
     useEffect(() => {
         const observer = createIntersectionObserver({
             options: {
                 root: rootRef.current,
+                threshold: 1,
+                rootMargin: "0px 0px 20px 0px",
             },
-            targets: [anchorRef.current],
+            targets: [bottomTargetRef.current, topTargetRef.current],
             onIntersecting: (entries) => {
-                setPageCeil((prev) => {
-                    if (prev + 1 >= row) {
-                        return row;
+                console.log(entries);
+                entries.forEach((entry) => {
+                    if (entry.target === topTargetRef.current) {
+                        setRange((prev) => {
+                            if (prev.startIndex - acceleration <= 0) {
+                                return {
+                                    startIndex: 0,
+                                    endIndex: prev.endIndex,
+                                };
+                            }
+                            return {
+                                startIndex: prev.startIndex - acceleration,
+                                endIndex: prev.endIndex,
+                            };
+                        });
+                    } else if (entry.target === bottomTargetRef.current) {
+                        setRange((prev) => {
+                            if (prev.startIndex + acceleration >= row) {
+                                return {
+                                    startIndex: row,
+                                    endIndex: prev.endIndex,
+                                };
+                            }
+                            return {
+                                startIndex: prev.startIndex,
+                                endIndex: prev.endIndex + acceleration,
+                            };
+                        });
                     }
-                    return prev + 1;
                 });
             },
         });
@@ -69,25 +108,28 @@ export const Vertical = ({ row = 1000000 }) => {
 
     return (
         <div className={"h-[200px] overflow-y-auto border"} ref={rootRef}>
-            {new Array(row)
-                .fill(0)
-                .slice(0, pageCeil)
-                .map((_, index) => {
-                    return (
-                        <div
-                            className={"h-[50px] border border-gray-300"}
-                            key={index}
-                        >
-                            {index}
-                        </div>
-                    );
-                })}
-
             <div
-                ref={anchorRef}
+                ref={topTargetRef}
                 className={"text-center text-sm text-gray-500"}
             >
-                {pageCeil === row ? "暂无更多" : "加载更多"}
+                {range.endIndex === row ? "暂无更多" : "下拉加载更多"}
+            </div>
+            {rows.map((item, index) => {
+                return (
+                    <div
+                        className={"h-[100px] border border-gray-300"}
+                        key={item}
+                    >
+                        {item}
+                    </div>
+                );
+            })}
+
+            <div
+                ref={bottomTargetRef}
+                className={"text-center text-sm text-gray-500"}
+            >
+                {range.endIndex === row ? "暂无更多" : "加载更多"}
             </div>
         </div>
     );
