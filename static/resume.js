ad444bc0a7006583b8d030b4bd8ef8b021c5ece3
marvin <sea-son@foxmail.com>
2025-03-21 11:00:20 +0800

    feat: resume

---
+++ static/resume.js
@@ -0,0 +1,95 @@
+const data = {
+    header: {
+        title: "马文澍-Web开发工程师",
+        university: "武汉科技大学",
+        contactInfo: [
+            { type: "phone", label: "📞 15623192717", href: "tel:+8615623192717" },
+            { type: "email", label: "📧 Mail", href: "mailto:2764876579@qq.com" },
+            { type: "website", label: "🌐 fuelstack.icu", href: "http://fuelstack.icu" },
+            { type: "github", label: "📥 GitHub: marvin-season", href: "https://github.com/marvin-season?tab=repositories" }
+        ]
+    },
+    sections: [
+        {
+            title: "核心优势",
+            items: [
+                {
+                    title: "开发经验",
+                    details: [
+                        "4年有效 Web开发经验",
+                        "1年有效Java开发经验",
+                        "1年有效小程序开发经验"
+                    ]
+                },
+                {
+                    title: "技术探索",
+                    details: [
+                        {
+                            description: "前端工程化 & 模块化, Vite(Rollup) + Babel + Mono-Repo",
+                            links: [
+                                { label: "👌 AIO-Modal", href: "https://www.npmjs.com/package/aio-modal" },
+                                { label: "🚀 Babel", href: "https://github.com/marvin-season/i18n-ast" }
+                            ]
+                        },
+                        {
+                            description: "文档网站搭建，知识积累",
+                            links: [
+                                { label: "🌐 WebSite", href: "http://fuelstack.icu" },
+                                { label: "🌐 github pages", href: "https://marvin-season.github.io/danny-website/" }
+                            ]
+                        },
+                        {
+                            description: "富文本组件定制化开发经验",
+                            links: [
+                                { label: "🌐 AI 智能书写demo", href: "http://ai.fuelstack.icu" },
+                                { label: "📥 GitHub", href: "https://github.com/marvin-season/ai-novel" }
+                            ]
+                        },
+                        {
+                            description: "擅长React相关技术栈开发: ReactHook & NextJs & 生态",
+                            links: [
+                                { label: "🪜 React原理", href: "https://jser.pro/ddir/rie?reactVersion=18.3.1&snippetKey=hq8jm2ylzb9u8eh468" },
+                                { label: "🪜 SSR ISR ...", href: "https://nextjs.org/learn/dashboard-app/static-and-dynamic-rendering" }
+                            ]
+                        },
+                        "跨平台开发经验 (Electron, UniApp)",
+                        "熟悉Vue3/2/UniApp 开发",
+                        {
+                            description: "工具库和组件库的开发和维护",
+                            links: [
+                                { label: "shadcn/", href: "https://ui.shadcn.com/" }
+                            ]
+                        },
+                        {
+                            description: "CMS 内容管理系统",
+                            links: [
+                                { label: "Strapi self-host", href: "https://github.com/marvin-season/strapi-non-commerical-starter" }
+                            ]
+                        },
+                        {
+                            description: "错误跟踪和监控平台",
+                            links: [
+                                { label: "sentry-react demo", href: "https://github.com/marvin-season/sentry-react" }
+                            ]
+                        },
+                        {
+                            description: "Web性能优化",
+                            links: [
+                                { label: "好的用户体验是最重要的", href: "https://developer.mozilla.org/en-US/docs/Web/Performance/Guides/Fundamentals" }
+                            ]
+                        },
+                        "开发过后端项目Express & SpringBoot"
+                    ]
+                },
+                {
+                    title: "知识的专业性与系统性",
+                    details: [
+                        "计算机科班： 本科学习信息安全，后转向软件开发",
+                        "重点本科一批： 武汉科技大学湖北高校排名第八",
+                        "计算机体系化学习： 操作系统 + 网络及其安全 + 数据结构与算法 + 数据库 + 高级编程语言（C/C++/Java）"
+                    ]
+                }
+            ]
+        }
+    ]
+};
