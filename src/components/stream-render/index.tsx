ad421b16be6da17e3e0360872f09f6d654859e90
marvin <sea-son@foxmail.com>
2025-03-27 18:12:06 +0800

    chore: 流数据优化

--- src/components/stream-render/index.tsx
+++ src/components/stream-render/index.tsx
@@ -1,11 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import { createIntersectionObserver, sleep } from "aio-tool";
 
-const s: string = `<think>The central idea of React's API is to think of updates as if they cause the entire app to re-render. This allows the developer to reason declaratively, rather than worry about how to efficiently transition the app from any particular state to another (A to B, B to C, C to A, and so on).
+import s from './s';
 
-Actually re-rendering the entire app on each change only works for the most trivial apps; in a real-world app, it's prohibitively costly in terms of performance. React has optimizations which create the appearance of whole app re-rendering while maintaining great performance. The bulk of these optimizations are part of a process called reconciliation.
-
-Reconciliation is the algorithm behind what is popularly understood as the "virtual DOM." A high-level description goes something like this: when you render a React application, a tree of nodes that describes the app is generated and saved in memory. This tree is then flushed to the rendering environment — for example, in the case of a browser application, it's translated to a set of DOM operations. When the app is updated (usually via setState), a new tree is generated. The new tree is diffed with the previous tree to compute which operations are needed to update the rendered app.`;
 export default function StreamRender() {
     const [content, setContent] = useState("");
     const [loading, setLoading] = useState(false);
@@ -82,9 +79,9 @@ export default function StreamRender() {
             >
                 clean
             </button>
-            <div className={"text-gray-600 p-4 text-xs"}>
+            <div className={"text-gray-600 text-xs"}>
                 <em>Note: 打开控制台检查正在输出的元素</em>
-                <div>
+                <div className={"h-[150px] overflow-y-scroll border"}>
                     <em>Full String:</em>
                     {s}
                 </div>
@@ -92,7 +89,7 @@ export default function StreamRender() {
             </div>
             <div
                 ref={rootRef}
-                className={"h-[50px] overflow-y-scroll border rounded text-gray-600 p-4"}
+                className={"h-[80px] overflow-y-scroll border rounded text-gray-600 p-4"}
             >
                 {/*<div className={"text-sm text-gray-500"}>{contentObject.think_content}</div>*/}
                 <div>
---
+++ src/components/stream-render/s.js
@@ -0,0 +1,27 @@
+const s = `
+What is reconciliation?
+reconciliation
+The algorithm React uses to diff one tree with another to determine which parts need to be changed.
+update
+A change in the data used to render a React app. Usually the result of \`setState\`. Eventually results in a re-render.
+The central idea of React's API is to think of updates as if they cause the entire app to re-render. This allows the developer to reason declaratively, rather than worry about how to efficiently transition the app from any particular state to another (A to B, B to C, C to A, and so on).
+
+Actually re-rendering the entire app on each change only works for the most trivial apps; in a real-world app, it's prohibitively costly in terms of performance. React has optimizations which create the appearance of whole app re-rendering while maintaining great performance. The bulk of these optimizations are part of a process called reconciliation.
+
+Reconciliation is the algorithm behind what is popularly understood as the "virtual DOM." A high-level description goes something like this: when you render a React application, a tree of nodes that describes the app is generated and saved in memory. This tree is then flushed to the rendering environment — for example, in the case of a browser application, it's translated to a set of DOM operations. When the app is updated (usually via setState), a new tree is generated. The new tree is diffed with the previous tree to compute which operations are needed to update the rendered app.
+
+Although Fiber is a ground-up rewrite of the reconciler, the high-level algorithm described in the React docs will be largely the same. The key points are:
+
+Different component types are assumed to generate substantially different trees. React will not attempt to diff them, but rather replace the old tree completely.
+Diffing of lists is performed using keys. Keys should be "stable, predictable, and unique."
+Reconciliation versus rendering
+The DOM is just one of the rendering environments React can render to, the other major targets being native iOS and Android views via React Native. (This is why "virtual DOM" is a bit of a misnomer.)
+
+The reason it can support so many targets is because React is designed so that reconciliation and rendering are separate phases. The reconciler does the work of computing which parts of a tree have changed; the renderer then uses that information to actually update the rendered app.
+
+This separation means that React DOM and React Native can use their own renderers while sharing the same reconciler, provided by React core.
+
+Fiber reimplements the reconciler. It is not principally concerned with rendering, though renderers will need to change to support (and take advantage of) the new architecture.
+`
+
+export default s
