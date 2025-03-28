b721c9ebc43a0cba7a2d74bc56720fb0600998ec
marvin <sea-son@foxmail.com>
2025-03-28 17:56:40 +0800

    feat: webkit-scrollbar

--- src/pages/ai/index.tsx
+++ src/pages/ai/index.tsx
@@ -2,6 +2,8 @@ import Layout from "@theme/Layout";
 
 export default function AI(){
     return  <Layout
+
+        wrapperClassName={"flex-shrink-0  h-[calc(100vh-64px)]"}
         title={`AI Helper`}
         description="Personal Information"
     >
