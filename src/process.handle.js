bfa640b365c3034d373a3fe9960e851132f4ce15
marvin <sea-son@foxmail.com>
2025-04-02 18:16:32 +0800

    feat: new_ js

--- src/process.handle.js
+++ src/process.handle.js
@@ -1,24 +1,16 @@
-const set = new Set();
-
-function publish(data) {
-    set.forEach((resolve) => {
-        resolve(data);
-    });
-    set.clear();
+function new_(func, ...params) {
+    const obj = Object.create(func.prototype);
+    const result = func.apply(obj, params);
+    return typeof result === "object" ? result : obj;
 }
 
-async function subscribe() {
-    const result = await new Promise((resolve) => {
-        set.add(resolve);
-    });
-    console.log("subscriber", result);
-    return subscribe();
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    console.log('this', this, Object.getPrototypeOf(this) === Person.prototype);
 }
 
-subscribe();
-subscribe();
-let i = 0;
-
-setInterval(() => {
-    publish(i++);
-}, 1000);
+const person1 = new Person("张三", 13);
+console.log("person1", person1);
+const person2 = new_(Person, "张三", 12);
+console.log("person2", person2);
