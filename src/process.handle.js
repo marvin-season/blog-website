6a6a407d35e4b470d18c86f310e2bef8f7d1c770
marvin <sea-son@foxmail.com>
2025-04-03 10:25:23 +0800

    feat: 防抖节流

--- src/process.handle.js
+++ src/process.handle.js
@@ -1,16 +1,18 @@
-function new_(func, ...params) {
-    const obj = Object.create(func.prototype);
-    const result = func.apply(obj, params);
-    return typeof result === "object" ? result : obj;
+function throttle(func, timeout = 500) {
+    let timer = null
+    return function(...props) {
+        if(timer) {
+            return
+        }
+        timer = setTimeout(() => {
+            func(...props);
+            timer = null
+        }, timeout)
+    }
 }
-
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
-    console.log('this', this, Object.getPrototypeOf(this) === Person.prototype);
+function func() {
+    console.log(Date.now())
 }
+const test = throttle(func)
+setInterval(test)
 
-const person1 = new Person("张三", 13);
-console.log("person1", person1);
-const person2 = new_(Person, "张三", 12);
-console.log("person2", person2);
