afe36b20b5f9428b3b7e6b6c897a45e38906d12c
marvin <sea-son@foxmail.com>
2025-04-30 11:13:06 +0800

    🐛 FIX: build

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -126,7 +126,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/effective',
             },
           ],
         },
